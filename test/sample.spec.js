9f939368dd99fe3beca57945e1d319182eca0dc5
probie <probie0@163.com>
2020-04-19 22:17:03 +0800

    完成对自己的代码的测试

--- test/sample.spec.js
+++ test/sample.spec.js
@@ -4,7 +4,7 @@ describe('add todo', function () {
 
     before (async function () {
       page = await browser.newPage();
-      await page.goto('http://127.0.0.1:7001/');
+      await page.goto('http://127.0.0.1:3000', {delay:3000});
     });
   
     after (async function () {
@@ -12,49 +12,54 @@ describe('add todo', function () {
     });
 
     it('should have correct title', async function() {
-        expect(await page.title()).to.eql('demo');
+        expect(await page.title()).to.eql('Todo List Demo');
     })
     it('should new todo correct', async function() {
-      await page.click('#new-todo', {delay: 500});
-      await page.type('#new-todo', 'new todo item', {delay: 50});
-      await page.keyboard.press("Enter");
-      let todoList = await page.waitForSelector('#todo-list');
-      const expectInputContent = await page.evaluate(todoList => todoList.lastChild.querySelector('label').textContent, todoList);
-      expect(expectInputContent).to.eql('new todo item');
+        const inputText = 'new todo item';
+      await page.click('#add-item input', {delay: 500});
+      await page.type('#add-item input', inputText, {delay: 50});
+      await page.focus('#add-item input')
+      await page.keyboard.press("Enter",{delay: 500});
+      const todoItem = await page.waitForSelector('#todo-list:last-child');
+      let expectInputContent = await page.evaluate(todoItem => todoItem.querySelector("span").textContent, todoItem);
+      expectInputContent = expectInputContent.slice(0, expectInputContent.length - 1);
+      expect(expectInputContent).to.eql(inputText);
     }) 
 
     it('should render all the item', async function(){
       for(let i = 0; i < 3; i++){
-        await page.click('#new-todo', {delay: 500});
-        await page.type('#new-todo', 'new todo item', {delay: 50});
-        await page.keyboard.press("Enter");
+        await page.click('#add-item input', {delay: 500});
+        await page.type('#add-item input', 'new todo item'+i, {delay: 50});
+        await page.focus('#add-item input')
+        await page.keyboard.press("Enter",{delay: 500});
       }
+      await delay(2000);
       let todoList = await page.waitForSelector('#todo-list');
-      const itemNum = await page.evaluate(todoList => todoList.childElementCount, todoList);
+      const itemNum = await page.evaluate(todoList => todoList.childElementCount-1, todoList);
       expect(itemNum).to.eql(4);
     })
 
 
     it('should change the status of item to \'completed\'', async function(){
-      let todoItem = await page.waitForSelector('#todo-list li:last-child');
-      let checkBtn = await todoItem.$('input.toggle');
+      let todoItem = await page.waitForSelector('#todo-list:last-child');
+      let checkBtn = await todoItem.$('.done-btn');
       await checkBtn.click();
       await delay(1000);
-      const status = await page.evaluate(todoItem => todoItem.className, todoItem);
-      expect(status).to.eql('completed');
+      const status = await page.evaluate(todoItem => todoItem.querySelector("span").className, todoItem);
+      expect(status).to.eql('done-item');
     })
 
     it('should delete the item', async function(){
-      let todoList = await page.waitForSelector('#todo-list');
-      let todoItem = await todoList.$('li:last-child');
-      let deleteBtn = await todoItem.$('button');
-      let react_id = await page.evaluate(todoItem => todoItem.getAttribute('data-reactid'), todoItem);
+      const todoItem = await page.waitForSelector('#todo-list:last-child');
+      const deleteBtn = await todoItem.$('.del-btn');
+      const item_id = await page.evaluate(todoItem => todoItem.getAttribute('itemid'), todoItem);
       await todoItem.hover();
       await deleteBtn.click();
       await delay(2000);
-      let todoItemDeleted = await page.$('#todo-list li[data-reactid=\'' + react_id + '\']');
+      let todoItemDeleted = await page.$('#todo-list[itemID=\'' + item_id + '\']');
       expect(todoItemDeleted).to.eql(null);
     })
+
 
 
 
